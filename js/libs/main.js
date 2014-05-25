e81c03100f0632d8de7e78ff159267ff01157b70
Matthew Hasbach <hasbach.git@gmail.com>
2014-05-25 07:35:25 -0400

    - Change to uncompressed KineticJS, reference: https://github.com/ericdrowell/KineticJS/issues/652
    - Fix dependency paths

--- js/libs/main.js
+++ js/libs/main.js
@@ -7,9 +7,9 @@ require.config({
         propertyParser: '../deps/requirejs-plugins-v1.0.3/propertyParser',
         buzz: '../deps/buzz-v1.1.0.min',
         jquery: '../deps/jquery-v1.11.1.min',
-        Kinetic: '../deps/kinetic-DEV',
+        Kinetic: '../deps/kinetic-v5.1.0',
         screenFull: '../deps/screenfull-v1.1.1.min',
-        underscore: '../deps/underscore-DEV',
+        underscore: '../deps/underscore-v1.6.0.min',
 
         // Modules
         assets: 'assets',
