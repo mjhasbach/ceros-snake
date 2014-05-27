f0b6edd49134053c8030f32ba4d2922339e752cb
Matthew Hasbach <hasbach.git@gmail.com>
2014-05-27 16:23:19 -0400

    Code style

--- js/libs/assets.js
+++ js/libs/assets.js
@@ -2,17 +2,15 @@ define([ 'audio', 'webFonts', 'menu', 'game' ],
     function( audio, webFonts, menu, game ){
         var assets = { menu: menu, game: game, audio: audio };
 
-        _.extend( assets, {
-            waitForAsync: function wait( cb ){
-                if ( audio.song.isLoaded && webFonts.areLoaded ) cb();
-                else setTimeout( wait, 100, cb )
-            },
+        assets.waitForAsync = function wait( cb ){
+            if ( audio.song.isLoaded && webFonts.areLoaded ) cb();
+            else setTimeout( wait, 100, cb )
+        };
 
-            init: _.once( function( options ){
-                assets.game.init( options );
-                assets.menu.init( options )
-            })
-        });
+        assets.init = function( options ){
+            assets.game.init( options );
+            assets.menu.init( options )
+        };
 
         return assets
     }
