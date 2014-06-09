e03a78b0d0f06bdc93ebf5a1afb1a153ec735914
Matthew Hasbach <hasbach.git@gmail.com>
2014-06-08 21:15:14 -0400

    Remove settings.game.counter

--- js/libs/settings.js
+++ js/libs/settings.js
@@ -143,15 +143,6 @@ define([], function() {
                 maximum: 10,
 
                 spawnProbability: 0.3
-            },
-
-            counter: {
-                font: { color: '#372a50' },
-
-                shadow: {
-                    color: '#4A374B',
-                    blur: 25
-                }
             }
         }
     };
