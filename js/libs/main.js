8b13ad7a3a7d1e2346b372dad01de9c473829d84
Matthew Hasbach <hasbach.git@gmail.com>
2014-06-14 15:19:08 -0400

    Add new dependencies and high scores module

--- js/libs/main.js
+++ js/libs/main.js
@@ -5,10 +5,12 @@ require.config({
         goog: '../deps/requirejs-plugins-v1.0.3/goog',
         font: '../deps/requirejs-plugins-v1.0.3/font',
         propertyParser: '../deps/requirejs-plugins-v1.0.3/propertyParser',
+        bigScreen: '../deps/BigScreen-mjhasbach-fork',
         buzz: '../deps/buzz-v1.1.0.min',
+        Firebase: '../deps/firebase-v1.0.15.min',
         jquery: '../deps/jquery-v1.11.1.min',
         Kinetic: '../deps/kinetic-v5.1.0',
-        bigScreen: '../deps/BigScreen-mjhasbach-fork',
+        kineticEditableText: '../deps/kinetic.editable',
         underscore: '../deps/underscore-v1.6.0.min',
 
         // Modules
@@ -17,6 +19,7 @@ require.config({
         background: 'background',
         events: 'events',
         game: 'game',
+        highScores: 'highScores',
         loading: 'loading',
         menu: 'menu',
         settings: 'settings',
