f8dcf6f0c5611bd525865a4507f748e7c68e83e1
Matthew Hasbach <hasbach.git@gmail.com>
2014-07-01 15:09:32 -0400

    Modify highScores.view.next.hitBox values

--- js/libs/highScores.js
+++ js/libs/highScores.js
@@ -318,10 +318,10 @@ define([ 'Kinetic', 'kineticEditableText', 'backbone', 'firebase', 'settings', '
                 }),
 
                 hitBox: new Kinetic.Rect({
-                    x: util.calculate.absolute.x( 2.518 ),
-                    y: util.calculate.absolute.y( 1.23 ),
-                    width: util.calculate.absolute.x( 12.39 ),
-                    height: util.calculate.absolute.y( 6.92 ),
+                    x: util.calculate.absolute.x( 1.681 ),
+                    y: util.calculate.absolute.y( 1.222 ),
+                    width: util.calculate.absolute.size( 13.2 ),
+                    height: util.calculate.absolute.size( 12.5 ),
                     opacity: 0
                 })
             },
